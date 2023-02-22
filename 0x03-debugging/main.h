33e37055a02a60e745a332efb35cc8edda797a42
Terrence <terrencegumani0@gmail.com>
2023-02-21 23:14:34 -0800

    Task 0-main.c, main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,14 +1,10 @@
-#ifndef "main.h"
+#ifndef MAIN_H
+#define MAIN.H
 #include <stdio.h>
-
-/** 
- * main - debugging
- *
- */
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* main.h */
+#endif
