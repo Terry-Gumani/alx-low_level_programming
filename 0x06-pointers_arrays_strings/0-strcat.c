d5c3deee113e8da8ca918e75ab053e1e11966027
Terrence <terrencegumani0@gmail.com>
2023-03-02 06:55:19 -0800

    Task 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, n;
+	int i;
+	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
+		i++;
 	}
-
-	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	j = 0;
+	while (src[j] != '\0')
 	{
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-
+	dest[i] = '\0';
 	return (dest);
 }
