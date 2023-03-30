2e97d218ea9bb153cf909f395ba8fddd96775355
Terry-Gumani <terrencegumani0@gmail.com>
2023-03-30 08:57:35 -0700

    Task 0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,6 +14,7 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ * * Description: singly linked list node structure
  */
 
 typedef struct list_s
