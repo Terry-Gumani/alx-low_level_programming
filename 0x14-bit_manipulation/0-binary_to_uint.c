d01c1c04e220f87fdddd463010790db81de6c7bb
Terry-Gumani <terrencegumani0@gmail.com>
2023-04-06 01:45:30 -0700

    Task 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,23 +9,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+  unsigned int decimal = 0;
 
-	if (b == '\0')
-		return (0);
+  if (b == NULL)
+    return (0);
 
-	for (len = 0; b[len];)
-		len++;
+  while (*b != '\0')
+  {
+    if (*b == '0' || *b == '1')
+    {
+      decimal = decimal << 1;
+      decimal += (*b - '0');
+      b++;
+    }
+    else
+    {
+      return (0);
+    }
+  }
 
-	for (len -= 1; len >= 0; len--)
-	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
-	}
-
-	return (num);
+  return (decimal);
 }
