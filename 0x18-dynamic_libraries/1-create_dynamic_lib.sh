a4052088add9d7c0c9502024c5d21e0aba7944a6
Terry-Gumani <terrencegumani0@gmail.com>
2023-06-20 07:32:23 -0700

    Completed File

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-gcc -fPIC -c *.c
-gcc -shared -o liball.so *.o
+gcc *.c -fPIC -shared -o liball.so
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -25,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif 
+#endif
