5c18ccf44e19507d920c0e6cd316ba23ea3b4c80
Terry-Gumani <terrencegumani0@gmail.com>
2023-06-20 06:14:17 -0700

    Completed File

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - puts string to stdout
+ *
+ * @str: string to print to stdout
+ *
+ * Return: always void
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
